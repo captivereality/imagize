989e8fa2641b8368ba8c4da7e7ee62d24ba0b93f
Mark Castle <mark.castle@inovus.org>
2022-06-27 19:22:18 +0100

    Removed: Url Checking Temporarily

--- Imagize/Controllers/ImageController.cs
+++ Imagize/Controllers/ImageController.cs
@@ -73,8 +73,8 @@ namespace Imagize.Controllers
         public async Task<string> Get([FromQuery] string uri)
         {
 
-            if (!await IsValidUri(uri))
-                return "Invalid Uri or FileType";
+            //if (!await IsValidUri(uri))
+            //    return "Invalid Uri or FileType";
 
             _logger.LogInformation("Get");
 
